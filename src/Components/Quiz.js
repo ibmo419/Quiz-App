4e2b82de28c0c2497415dbdd8251146cb5d316a0
Mohamed <hadjamor.medali@gmail.com>
2021-05-11 08:24:59 -0700

    Final Commit

---
+++ src/Components/Quiz.js
@@ -0,0 +1,110 @@
+import React ,{useState,useEffect}from 'react'
+import {QuizData} from './QuizData'
+
+function Quiz() {
+    const [state, setstate] = useState(
+        {
+            userAnswer:null,
+            score:0,     //current users answer
+            currentIndex:0,  //current questions index
+            options: [],       //the four options
+            quizEnd: false, //True if it's the last question
+                         //the Score
+            disabled: true,
+        }
+    )
+    
+    const LoadQuiz=()=>{
+        const {currentIndex}=state;
+        setstate({...state,
+            question:QuizData[currentIndex].question,
+            options:QuizData[currentIndex].options,
+            answer:QuizData[currentIndex].answer,
+           
+        }  
+        );
+    }
+    
+    const checkAnswer=answer=>{
+        setstate({...state,
+            userAnswer:answer,
+            disabled:false
+        })
+    }
+
+    const nextQuestionHandler=()=>{
+        const {userAnswer,answer,score,currentIndex}=state;
+        if(userAnswer===answer){
+            setstate({...state,
+                       score:score+1,
+                       currentIndex:currentIndex+1,
+                       userAnswer:null})
+                      
+        }else{
+        setstate({...state,
+                  currentIndex:currentIndex+1,
+                  userAnswer:null
+                 })
+
+        }
+                 
+    }
+    
+    const finishHandler =() => {
+        const {userAnswer,answer,score}=state;
+        if(state.currentIndex === QuizData.length -1){
+            if(userAnswer===answer){
+                setstate({...state,
+                           score:score+1,
+                           quizEnd:true})
+                          
+            }else{
+                setstate({...state,
+                    quizEnd:true
+                })
+            }
+        }
+    
+    }
+    useEffect(() => LoadQuiz()
+    ,[state.currentIndex])
+
+    return (
+        <div>
+            {
+                state.quizEnd?
+                <div>
+                    <h1>Game Over Finale score is {state.score} points</h1>
+                    <p>The correct answers for the quiz are </p>
+                    <ul>
+                        {QuizData.map((item,index)=>(
+                            <li className="options" key={index} >{item.answer}</li>
+                        ))}
+                    </ul>
+                </div>:
+            <div>
+            <h2>{state.question}</h2>
+            <span>{`Question ${state.currentIndex+1} of ${QuizData.length}`}</span>
+            {
+                state.options.map((option,index)=>
+                    <p  key={index} className={`options ${state.userAnswer===option? "selected":null}`}
+                    onClick={()=>checkAnswer(option)}
+                    >{option}</p>
+                )
+            }
+            {
+                state.currentIndex<QuizData.length-1 ?
+                <button disabled={state.disabled} onClick={nextQuestionHandler}>
+                    Next Question
+                </button>:
+                <button onClick={finishHandler} >
+                    Finish
+                </button>
+            }
+        </div>
+        }
+        </div>
+    )
+}
+
+export default Quiz
---
+++ src/Components/QuizData.js
@@ -0,0 +1,32 @@
+ export const QuizData = [
+    {
+        id: 0,
+        question: `What is the capital of Romania?`,
+        options: [`New Delhi`, `Bucharest`, `Prague`, `Budapest`],
+        answer: `Bucharest`
+    },
+    {
+        id: 1,
+        question: `What is the capital of India?`,
+        options: [`Punjab`, `Awka`, `Owerri`, `New Delhi`],
+        answer: `New Delhi`
+    },
+    {
+        id: 2,
+        question: `What is the capital of Australia?`,
+        options: [`Mumbai`, `Abuja`, `Warri`, `Sydney`],
+        answer: `Sydney`
+    },
+    {
+        id: 3,
+        question: `What is the capital of Turkey?`,
+        options: [`Bangalore`, `Ankara`, `Istanbul`, `Enugu`],
+        answer: `Ankara`
+    },
+    {
+        id: 4,
+        question: `What is the capital of Ukrain?`,
+        options: [`Lviv`, `Odesa`, `Kiev`, `Kharkiv`],
+        answer: `Kiev`
+    },
+]
